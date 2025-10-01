47100f7e4af9eed53198bf7e7a86a4e8e5a2db8c
Aditya Singh <adityasinghrajput755@gmail.com>
2025-10-02 00:55:05 +0530

    feat: add LightRays component for enhanced visual effects and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import { BackgroundBeamsDemo } from "../sections/background-beams";
-
+import LightRays from "@/components/LightRays";
 export default function Home() {
   return (
-    <div>
-       <BackgroundBeamsDemo />
-
-    </div>
+    <div style={{ width: "100%", height: "600px", position: "relative" }}>
+      <LightRays
+        raysOrigin="top-center"
+        raysColor="#3333ff"
+        raysSpeed={1.5}
+        lightSpread={0.8}
+        rayLength={1.2}
+        followMouse={true}
+        mouseInfluence={0.1}
+        noiseAmount={0.1}
+        distortion={0.05}
+        className="custom-rays"
+      />
+      </div>
   );
 }
