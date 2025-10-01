6fb505288df76318b28e1624af92d04ec44c80a5
Aditya Singh <adityasinghrajput755@gmail.com>
2025-10-02 01:47:16 +0530

    Implement code changes to enhance functionality and improve performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,31 @@
 import LightRays from "@/components/LightRays";
+import Image from 'next/image';
 export default function Home() {
   return (
-    <div style={{ width: "100%", height: "600px", position: "relative" }}>
-      <LightRays
-        raysOrigin="top-center"
-        raysColor="#3333ff"
-        raysSpeed={1.5}
-        lightSpread={0.8}
-        rayLength={1.2}
-        followMouse={true}
-        mouseInfluence={0.1}
-        noiseAmount={0.1}
-        distortion={0.05}
-        className="custom-rays"
-      />
+    <div className="wrapper">
+      <div style={{ width: "100%", height: "1000px", position: "relative" }}>
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#3333ff"
+          raysSpeed={1.5}
+          lightSpread={0.8}
+          rayLength={1.2}
+          followMouse={true}
+          mouseInfluence={0.1}
+          noiseAmount={0.1}
+          distortion={0.05}
+          className="custom-rays"
+        ></LightRays>
       </div>
+      <div style={{ position: "absolute", inset: 1, zIndex: 0, overflow: "hidden" }}>
+        <Image
+          src="/images/finalHeroImage.webp"
+          alt="Background"
+          fill
+          style={{ objectFit: "cover" }}
+          priority
+        />
+      </div>
+    </div>
   );
 }
