47f45e45348d7cddde5de09b9551994068dff814
Aditya Singh <adityasinghrajput755@gmail.com>
2025-10-02 17:37:40 +0530

    Add bgBlur component for background image with blur effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,24 @@ import Navbar from "@/components/Navbar";
 import ImageBackground from "@/components/ui/ImageBackground";
 import LightRaysContainer from "@/components/ui/LightRays";
 import SplitTextWrapper from "@/components/ui/SplitText";
+import BgBlur from "@/components/ui/bgBlur";
+
 export default function Home() {
   return (
     <div className="wrapper">
       <div style={{ width: "100%", height: "1370px", position: "relative" }}>
         <LightRaysContainer />
+        <BgBlur />
         <ImageBackground />
         <div
           style={{
             position: "fixed",
-            top: "0%", // vertically center
-            left: "50%", // horizontally center
-            transform: "translate(-50%, -50%)", // shift back by half width & height
-            zIndex: 10, // on top of other elements
-            width: "50%", // optional: span half of parent width
-            textAlign: "center", // center text inside
+            top: "0%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: 10,
+            width: "50%",
+            textAlign: "center",
           }}
         >
           <Navbar />
@@ -24,12 +27,12 @@ export default function Home() {
         <div
           style={{
             position: "absolute",
-            top: "20%", // vertically center
-            left: "55%", // horizontally center
-            transform: "translate(-50%, -50%)", // shift back by half width & height
-            zIndex: 10, // on top of other elements
-            width: "50%", // optional: span half of parent width
-            textAlign: "center", // center text inside
+            top: "20%",
+            left: "55%",
+            transform: "translate(-50%, -50%)",
+            zIndex: 10,
+            width: "50%",
+            textAlign: "center",
           }}
         >
           <SplitTextWrapper />
---
+++ src/components/ui/bgBlur.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+export default function bgBlur() {
+  return (
+    <div style={{ position: "absolute", inset: 0, zIndex: 0, overflow: "hidden" }}>
+      <Image
+        src="/images/bgBlur.webp"
+        alt="Background"
+        fill
+        priority
+            style={{
+              objectFit: "cover",
+              objectPosition: "bottom", 
+            }}
+          />
+        </div>
+  );
+}
